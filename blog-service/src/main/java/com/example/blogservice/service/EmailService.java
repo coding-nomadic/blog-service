8146d86128d153e13e9c0add1e68b0d9c0c3d9ce
Tenzin Dawa <tnzngdw@gmail.com>
2023-03-24 09:24:02 +0530

    Delete blog-service directory

--- blog-service/src/main/java/com/example/blogservice/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.example.blogservice.service;
-
-import com.example.blogservice.models.RegistrationRequest;
-
-public interface EmailService {
-
-    void sendMail(String emailTo,String emailText);
-
-}
