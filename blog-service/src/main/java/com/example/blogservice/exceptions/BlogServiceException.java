739c788912d0807a7289da5685205db2e7b07550
Tenzin Dawa <tnzngdw@gmail.com>
2023-05-27 12:46:28 -0400

    Update BlogServiceException.java

--- blog-service/src/main/java/com/example/blogservice/exceptions/BlogServiceException.java
+++ blog-service/src/main/java/com/example/blogservice/exceptions/BlogServiceException.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 
 public class BlogServiceException extends RuntimeException {
-    private String message;
-    private String errorCode;
+
 
     public BlogServiceException(String message, Throwable cause) {
         super(message, cause);
@@ -15,7 +14,5 @@ public class BlogServiceException extends RuntimeException {
 
     public BlogServiceException(String message, String errorCode) {
         super(message);
-        this.message = message;
-        this.errorCode = errorCode;
     }
 }
